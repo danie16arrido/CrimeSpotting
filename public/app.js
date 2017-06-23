23930ff1b12bde14c9611415465663613a759615
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-23 18:04:23 +0100

    create function to update marker

--- public/app.js
+++ public/app.js
@@ -2,7 +2,8 @@ var initialize = function () {
   var mapDiv = document.getElementById('main-map')
 
   var center = { lat: 41.878114, lng: -87.629798 };
-  var mainMap = new MapWrapper( mapDiv, center, 10 )
+  var mainMap = new MapWrapper( mapDiv, center, 14 )
+  mainMap.addClickEvent();
 }
 
 window.addEventListener('load', initialize);
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -3,4 +3,28 @@ var MapWrapper = function(container, coords, zoom){
     center: coords,
     zoom: zoom
   });
+
+  this.marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap,
+    animation: google.maps.Animation.DROP
+  });
+}
+
+MapWrapper.prototype = {
+
+  updateMarker: function(coords){
+    this.marker.setPosition( coords )
+  },
+
+  addClickEvent: function(){
+      google.maps.event.addListener(this.googleMap, 'click', function(event){
+        var position = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+        this.updateMarker(position);
+      }.bind(this));
+  },
+
+  addCircleAroundSelectedPosition: function () {
+
+  }
 }
